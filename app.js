6f22192d4fdae15eee249bcb7b3c32078b9f12ca
Hayden Riebe <hjriebe@alaska.edu>
2025-07-30 12:49:40 -0800

    test CI/CD

--- app.js
+++ app.js
@@ -707,4 +707,5 @@ http.createServer(app).listen(config.PORT, function () {
   console.log('[DEBUG] CLIENT_ID:', clientID);
   console.log('[DEBUG] CLIENT_SECRET:', clientSecret ? '[REDACTED]': 'NOT SET');
   console.log('[DEBUG] REDIRECT_URI:', redirectURI);
+  console.log('Great Work!!');
 });
