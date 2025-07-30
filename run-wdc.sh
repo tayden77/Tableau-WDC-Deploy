358a14b8a699e4f520caea3c658cb203dc64e4b7
Hayden Riebe <hjriebe@alaska.edu>
2025-07-30 13:54:53 -0800

    good

--- run-wdc.sh
+++ run-wdc.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Building Docker image..."
+echo "🔧 Rebuilding Docker image for renxt-wdc..."
 docker build -t renxt-wdc .
 
-echo "Running container..."
-docker run --rm --env-file .env -p 3333:3333 renxt-wdc
+if [ $? -eq 0 ]; then
+  echo "🚀 Running container on http://localhost:3333"
+  docker run --rm --env-file .env -p 3333:3333 renxt-wdc
+else
+  echo "❌ Build failed. Container not started."
+fi
